743d6eb23a9438e5fa9aef2113516a73de621986
kh-ul <kinghand@ultralisk.net>
2018-02-25 23:50:22 +0800

    deploy code

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-echo build solPack ...
+echo build solpack ...
 go build 
 echo complete : now at "$(pwd)"
-echo "  $(ls -al --color | grep solPack*)"
-echo move solPack ... "=> $(echo $GOPATH  /bin)"
-mv solPack* $GOPATH/bin
+echo "  $(ls -al --color | grep solpack*)"
+echo move solpack ... "=> $(echo $GOPATH  /bin)"
+mv solpack* $GOPATH/bin
 echo complete : now at $GOPATH/bin/
-echo "  $(ls -al --color $GOPATH/bin | grep solPack*)"
+echo "  $(ls -al --color $GOPATH/bin | grep solpack*)"
